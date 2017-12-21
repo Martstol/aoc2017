93b33b44e272d7ab0d73b543eff499539fd15612
Martin <martstol@gmail.com>
2017-12-21 02:35:20 +0100

    Solve day 3 part 2

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import Day1 (solvePart1, solvePart2)
 import Day2 (solvePart1, solvePart2)
-import Day3 (solvePart1)
+import Day3 (solvePart1, solvePart2)
 
 getInputForDay :: Int -> String
 getInputForDay n = "res/day" ++ (show n) ++ ".txt"
@@ -10,4 +10,4 @@ getInputForDay n = "res/day" ++ (show n) ++ ".txt"
 main :: IO ()
 main = do
     content <- readFile (getInputForDay 3)
-    print (Day3.solvePart1 content)
+    print (Day3.solvePart2 content)
--- src/Day3.hs
+++ src/Day3.hs
@@ -1,6 +1,8 @@
-module Day3 (solvePart1) where
+module Day3 (solvePart1, solvePart2) where
 
 import Data.Complex
+import Data.Map (Map, (!))
+import qualified Data.Map as Map
 
 createMovementList :: [Complex Double] -> [Int] -> [Complex Double]
 createMovementList (fst:snd:tail) (x:xs) = (replicate x fst) ++ (replicate x snd) ++ (createMovementList tail xs)
@@ -12,9 +14,44 @@ positions :: [Complex Double]
 positions = scanl (+) (0 :+ 0) movementList
 
 manhattenDistance :: Complex Double -> Int
-manhattenDistance c = round ((abs (realPart c)) + (abs (imagPart c)))
+manhattenDistance c = round (sum (fmap abs c))
 
 solvePart1 :: String -> Int
 solvePart1 input = let
         n = read input
     in manhattenDistance (positions !! (n-1))
+
+
+solvePart2 :: String ->  Int
+solvePart2 input = let
+        n = read input
+    in (populateMap n)
+
+populateMap :: Int -> Int
+populateMap n = let
+        foobar (p:ps) m = let
+                v = eval p m
+            in if v <= n 
+                then foobar ps (Map.insert p v m) 
+                else v
+    in foobar positions Map.empty
+
+eval :: (Complex Double) -> Map (Complex Double) Int -> Int
+eval c m
+    | Map.null m = 1
+    | otherwise  = sum (map snd (neighbours c m))
+
+neighbours :: (Complex Double) -> Map (Complex Double) Int -> [((Complex Double), Int)]
+neighbours (a :+ b) m = let
+        member k = Map.member k m
+        entry k = (k, m ! k)
+        neighbourhood = [(a+x) :+ (b+y) | x <- [-1, 0, 1], y <- [-1, 0, 1], x /= 0 || y /= 0]
+    in map entry (filter member neighbourhood)
+
+instance (Ord a) => Ord (Complex a) where
+    (a :+ b) `compare` (c :+ d)
+        | a > c = GT
+        | a < c = LT
+        | a == c && b > d = GT
+        | a == c && b < d = LT
+        | a == c && b == d = EQ
