6928065bf7ff995f7ae8b4e9a99c68c22d95f269
Martin <martstol@gmail.com>
2018-04-14 20:44:58 +0200

    added tests for day 5 input

--- test/Day5Test.hs
+++ test/Day5Test.hs
@@ -13,3 +13,16 @@ spec_day5_solvePart2_examples :: Spec
 spec_day5_solvePart2_examples = do
     it "Day 5 part 2 for 0\n3\n0\n1\n-3" $
         solvePart2 "0\n3\n0\n1\n-3" `shouldBe` 10
+
+spec_day5_solvePart1_input :: Spec
+spec_day5_solvePart1_input = do
+    input <- runIO (readFile "res/day5.txt")
+    it "Day 5 part 1 for input" $
+        solvePart1 input `shouldBe` 394829
+
+
+spec_day5_solvePart2_input :: Spec
+spec_day5_solvePart2_input = do
+    input <- runIO (readFile "res/day5.txt")
+    it "Day 5 part 2 for input" $
+        solvePart2 input `shouldBe` 31150702
