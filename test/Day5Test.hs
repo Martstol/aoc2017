aa0279975bd476568de55c859142233a02d81971
Martstol <martstol@gmail.com>
2018-05-17 17:03:06 +0200

    Fixed test case labels for day 5

--- test/Day5Test.hs
+++ test/Day5Test.hs
@@ -6,12 +6,12 @@ import           Test.Tasty.Hspec
 
 spec_day5_solvePart1_examples :: Spec
 spec_day5_solvePart1_examples = do
-    it "Day 5 part 1 for 0\n3\n0\n1\n-3" $
+    it "Day 5 part 1 for 0 3 0 1 -3" $
         solvePart1 "0\n3\n0\n1\n-3" `shouldBe` 5
 
 spec_day5_solvePart2_examples :: Spec
 spec_day5_solvePart2_examples = do
-    it "Day 5 part 2 for 0\n3\n0\n1\n-3" $
+    it "Day 5 part 2 for 0 3 0 1 -3" $
         solvePart2 "0\n3\n0\n1\n-3" `shouldBe` 10
 
 spec_day5_solvePart1_input :: Spec
